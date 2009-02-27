9eefebf4a37bce81b37a5f986d8554d0b9fdd450
rjurney <rjurney@lucision.com>
2009-02-27 16:41:42 -0500

    Just got these commands to work between 2 EC2 instances, so this should work.  Have to cd to ./ebin and then ../bin/start.sh

--- ec2/launch_cluster.pl
+++ ec2/launch_cluster.pl
@@ -155,7 +155,7 @@ while ($booted == 0)
                   . $cluster_name
                   . ".pem root@"
                   . $instance->dns_name
-                  . " 'pwd;cd dubdub;pwd;git pull;ls;make;ls ./ebin;cd ./bin;./start.sh -n boot'";
+                  . " 'pwd;cd dubdub;pwd;git pull;ls;make;ls ./ebin;cd ./ebin;../bin/start.sh -n boot'";
                 print "Command: $git\n";
                 print `$git`;
 
@@ -218,7 +218,8 @@ if ($cluster_size > 1)
 
                 if ($instance->instance_state->name eq 'running')
                 {
-
+                    sleep 5;
+                    
                     # Increment to get a unique worker ID for this instance
                     $worker_counter++;
 
@@ -229,7 +230,7 @@ if ($cluster_size > 1)
                       . $cluster_name
                       . ".pem root@"
                       . $instance->dns_name
-                      . " 'pwd;cd dubdub;pwd;git pull;ls;make;ls ./ebin;cd ./bin;./start.sh -n worker$worker_counter -m $master_nodename'";
+                      . " 'pwd;cd dubdub;pwd;git pull;ls;make;ls ./ebin;cd ./ebin;../bin/start.sh -n worker$worker_counter -m $master_nodename'";
                     print "Command: $git\n";
                     print `$git`;
 
