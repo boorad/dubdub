ef8f02db64830cdf5aeca89b3f13e363d1c2e399
rjurney <rjurney@lucision.com>
2009-02-27 21:08:46 -0500

    Added -detached to detach erlang shell so script doesn't hang.

--- bin/start.sh
+++ bin/start.sh
@@ -54,9 +54,9 @@ done
 # Feed master nodename to slaves
 if test -z "$MASTER"
 then
-    echo "erl -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell"
-    erl -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell &2>1 >/dev/null &
+    echo "erl -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell -detached &2>1 >/dev/null &"
+    erl -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell -detached &2>1 >/dev/null &
 else
-    echo "erl -m $MASTER -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell"
-    erl -m $MASTER -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell &2>1 >/dev/null &
+    echo "erl -m $MASTER -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell -detached &2>1 >/dev/null &"
+    erl -m $MASTER -name $NAME -s dubdub_app -setcookie mysecretcookie -noshell -detached &2>1 >/dev/null &
 fi
